4e714a6a9d7a8ba4ccc8c141c9a484e79ce75490
Huy Dinh Thanh <huy.dinh@napaglobal.com>
2023-02-17 11:18:31 +0700

    change file format assets.

--- lib/common/assets/app_assets.dart
+++ lib/common/assets/app_assets.dart
@@ -3,9 +3,10 @@ import '../constants/constants.dart';
 abstract class AppAssets {
   AppAssets._();
 
-  static const illustrationQRScan = '$appAssetImageSource/ill_qr_scan.svg';
-  static const illustrationFaceID = '$appAssetImageSource/ill_face_id.svg';
-  static const illustrationShoppingItems =
-      '$appAssetImageSource/ill_shopping_items.svg';
+  static const imageQRScan = '$appAssetImageSource/image_qr_scan.png';
+  static const imageFaceID = '$appAssetImageSource/image_face_id.png';
+  static const imageShoppingItems = '$appAssetImageSource/image_shopping_items';
+  static const imagePolygonSuccess = '$appAssetImageSource/polygon_success.png';
+
   static const iconTea = '$appAssetIconSource/icon_tea.svg';
 }
