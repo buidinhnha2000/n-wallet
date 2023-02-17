28ddd3e5a6088cdd52aa83e8382a0facc1beb84a
Huy Dinh Thanh <huy.dinh@napaglobal.com>
2023-02-17 10:45:27 +0700

    import assets

--- lib/common/assets/app_assets.dart
+++ lib/common/assets/app_assets.dart
@@ -1,5 +1,11 @@
+import '../constants/constants.dart';
+
 abstract class AppAssets {
   AppAssets._();
 
-  static const sampleAsset = 'assets/...';
+  static const illustrationQRScan = '$appAssetImageSource/ill_qr_scan.svg';
+  static const illustrationFaceID = '$appAssetImageSource/ill_face_id.svg';
+  static const illustrationShoppingItems =
+      '$appAssetImageSource/ill_shopping_items.svg';
+  static const iconTea = '$appAssetIconSource/icon_tea.svg';
 }
