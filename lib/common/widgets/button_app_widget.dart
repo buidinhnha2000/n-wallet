8137a8d225b7484efbd4c8b23e8f50b47625dc66
Huy Dinh Thanh <huy.dinh@napaglobal.com>
2023-03-01 10:41:39 +0700

    implement common button

---
+++ lib/common/widgets/button_app_widget.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../theme/app_color.dart';
+import '../extensions/context.dart';
+
+enum ButtonType { onlyIcon, onlyText, iconAndText }
+
+class ButtonAppWidget extends StatelessWidget {
+  const ButtonAppWidget({
+    Key? key,
+    this.text,
+    this.icon,
+    required this.onPressed,
+    this.color,
+    required this.buttonType,
+    this.imageIcon,
+  }) : super(key: key);
+
+  final IconData? icon;
+  final Function()? onPressed;
+  final Color? color;
+  final ButtonType buttonType;
+  final String? text;
+  final String? imageIcon;
+
+  Widget? handleShowChildElement(BuildContext context,
+      {required ButtonType buttonType}) {
+    Widget? widget;
+    switch (buttonType) {
+      case ButtonType.onlyIcon:
+        widget = SvgPicture.asset(
+          imageIcon ?? '',
+        );
+        break;
+      case ButtonType.onlyText:
+        widget = Text(
+          text ?? '',
+          style: context.textTheme.titleMedium?.copyWith(
+            color: AppColors.textWhite,
+            fontSize: 16,
+          ),
+        );
+        break;
+      case ButtonType.iconAndText:
+        widget = _ButtonIconAndTextWidget(
+          imageIcon: imageIcon ?? '',
+          text: text ?? '',
+        );
+        break;
+    }
+    return widget;
+  }
+  static const double _onlyIconSize = 40;
+  double? handleChangeHeight() {
+    const double defaultHeight = 56;
+
+    if(buttonType == ButtonType.onlyIcon) {
+      return _onlyIconSize;
+    }
+    return defaultHeight;
+  }
+  double? handleChangeWidth() {
+
+    if(buttonType == ButtonType.onlyIcon) {
+      return _onlyIconSize;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: handleChangeHeight(),
+      width: handleChangeWidth(),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(const EdgeInsets.all(8.0)),
+          elevation: const MaterialStatePropertyAll(0),
+          foregroundColor:
+          MaterialStateProperty.all<Color>(AppColors.textWhite),
+          backgroundColor: MaterialStateProperty.all<Color?>(color),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(16.0)),
+              side: BorderSide(color: AppColors.buttonBorder, width: 2),
+            ),
+          ),
+        ),
+        onPressed: () {
+          onPressed?.call();
+        },
+        child: handleShowChildElement(context, buttonType: buttonType),
+      ),
+    );
+  }
+}
+
+class _ButtonIconAndTextWidget extends StatelessWidget {
+  const _ButtonIconAndTextWidget(
+      {required this.text, required this.imageIcon});
+
+  final String text;
+  final String imageIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 38,
+                width: 38,
+                decoration: const BoxDecoration(
+                    color: AppColors.buttonWhite,
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                child: SvgPicture.asset(
+                  imageIcon,
+                  height: 18,
+                  width: 9.34,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Expanded(flex: 1, child: SizedBox()),
+              Expanded(
+                flex: 6,
+                child: Text(
+                  text,
+                  style: context.textTheme.titleMedium?.copyWith(
+                    color: AppColors.textBlack,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/common/widgets/widgets.dart
+++ lib/common/widgets/widgets.dart
@@ -1,5 +1,3 @@
-export 'button/button_check.dart';
-export 'button/button_option.dart';
 export 'form_text/icon_show_password.dart';
 export 'form_text/text_form.dart';
 export 'form_text/validation_form.dart';
