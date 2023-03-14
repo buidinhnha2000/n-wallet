6ad38ff47b6bb0e64a718212c8e15c8de4aa5169
Huy Dinh Thanh <huy.dinh@napaglobal.com>
2023-03-14 09:43:47 +0700

    fixe: initializeApp firebase

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,19 +1,14 @@
 import 'package:dio/dio.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app_flavor.dart';
 import 'data/remote/remote.dart';
 import 'di/service_locator.dart';
-import 'firebase_options.dart';
 import 'initialize.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   final serviceLocator = ServiceLocator.instance;
   serviceLocator.configureLocalStorage(
